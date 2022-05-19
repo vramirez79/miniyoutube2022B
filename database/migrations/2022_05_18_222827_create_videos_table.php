5d80e5c387cbd6afc2a9fc93136e6c025665ce73
Victor Ramirez <vramirez@utj.edu.mx>
2022-05-19 16:52:13 -0500

    Primer commit

---
+++ database/migrations/2022_05_18_222827_create_videos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title',150);
+            $table->string('description',250);
+            $table->string('image',150);
+            $table->string('video_path',150);
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+};
